5b6ec11707357e30cc24299338eceedcd20c4274
Edwin Ariza <me@edwinariza.com>
2020-08-08 09:40:25 -0500

    MODFUN: Modificación de la estructura para incluir autoload y namespace.

--- src/JMail.php
+++ src/JMail.php
@@ -2,33 +2,35 @@
 namespace Systemico;
 
 use Mailgun\Mailgun;
+use \Mailjet\Resources;
 
 /**
  * Clase encargada del envió masivo de correos.
  */
-
-
 class JMail{
     private $api_key;
     private $domain;
     private $sender;
     private $service;
-    private $name;
+    private $name="";
+    private $name_to="";
     public static $MAILGUN='MAILGUN';
+    public static $MAILJET='MAILJET';
   /**
    * Permite la definición de credenciales requeridas para la operación del la librería.
-   * @param $api Key del servicio a utilizar
+   * @param $api Key del servicio a utilizar, puede ser array
    * @param $domain Dominio al que está vinculada la petición.
    * @param $sender Información de quien remite el correo, y que aparecerá como remisor.
    * @param $service --> Tipo de servicio a utilizar JMail::$MAILGUN
    * @param $name Nombre de la cuenta que remite el correo.
    */
-    public function credentials($api_key, $domain, $sender, $service, $name=""){
+    public function credentials($api_key, $domain, $sender, $service, $name="", $name_to=""){
       $this->api_key=$api_key;
       $this->domain=$domain;
       $this->sender=$sender;
       $this->service=$service;
       $this->name=$name;
+      $this->name_to=$name_to;
     }
   /**
    * @param $email_to Destinatario del mensaje.
@@ -41,6 +43,23 @@ class JMail{
    * @throws \Mailgun\Messages\Exceptions\MissingRequiredMIMEParameters
    */
     public function send($email_to,$subject,$content,$altbody="", $tag=""){
+      if($this->service == JMail::$MAILGUN){
+        $this->send_mailgun($email_to,$subject,$content,$altbody,$tag);
+      }
+      if($this->service == JMail::$MAILJET){
+        $this->send_mailjet($email_to,$subject,$content,$altbody,$tag);
+      }
+    }
+
+  /**
+   * @param $email_to Destinatario del mensaje.
+   * @param $subject  Asunto del mensaje
+   * @param $content Contenido del mensaje
+   * @param string $altbody Mensaje altbody, aparece en el preview del mensaje
+   * @param string $name Nombre del remitente en caso que se quiera personalizar como Carlos Ariza <carlos.ariza@systemico.co>
+   * @param string $tag Etiqueta para marcar el correo.
+   */
+    public function send_mailgun($email_to,$subject,$content,$altbody="", $tag=""){
       $mg = Mailgun::create($this->api_key);
       $domain = $this->domain;
       $params = array(
@@ -52,5 +71,38 @@ class JMail{
       );
       $mg->messages()->send($domain, $params);
     }
+  /**
+   * @param $email_to Destinatario del mensaje.
+   * @param $subject  Asunto del mensaje
+   * @param $content Contenido del mensaje
+   * @param string $altbody Mensaje altbody, aparece en el preview del mensaje
+   * @param string $name Nombre del remitente en caso que se quiera personalizar como Carlos Ariza <carlos.ariza@systemico.co>
+   * @param string $tag Etiqueta para marcar el correo.
+   */
+  public function send_mailjet($email_to,$subject,$content,$altbody="", $tag=""){
+    echo "Enviando con MailJet";
+    $mj = new \Mailjet\Client($this->api_key[0],$this->api_key[1],true,['version' => 'v3.1']);
+    $body = [
+      'Messages' => [
+        [
+          'From' => [
+            'Email' => $this->sender,
+            'Name' => $this->name
+          ],
+          'To' => [
+            [
+              'Email' => $email_to,
+              'Name' => $this->name_to
+            ]
+          ],
+          'Subject' => $subject,
+          'TextPart' => $altbody,
+          'HTMLPart' => $content,
+          'CustomID' => $tag
+        ]
+      ]
+    ];
+    $response = $mj->post(Resources::$Email, ['body' => $body]);
+    $response->success() && var_dump($response->getData());
+  }
 }
-?>
--- test/test.php
+++ test/test.php
@@ -10,5 +10,11 @@ require_once (__DIR__."/../vendor/autoload.php");
 use Systemico\JMail;
 
 $jmail= new JMail();
+
+// Mensaje con MailGUN
 $jmail->credentials('[PRIVATE_API_KEY]', '[DOMAIN]', '[SENDER]',JMail::$MAILGUN,'[NAME_SENDER]');
+$jmail->send('[EMAIL_TO]','Hello World!','My firts Email HTML','My first mail TEXT');
+
+// Mensaje con MailJet
+$jmail->credentials(array('[API_KEY]','[SECRET_API_KEY]'), '[DOMAIN]', '[SENDER]',JMail::$MAILJET,'[NAME_SENDER]');
 $jmail->send('[EMAIL_TO]','Hello World!','My firts Email HTML','My first mail TEXT');
